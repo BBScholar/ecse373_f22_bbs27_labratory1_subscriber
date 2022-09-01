535037981cfda94e1d1dc93b2769c6362971822c
Ben Scholar <bbs27@case.edu>
2022-08-31 20:25:20 -0400

    added publisher_package as dep, utilized topic_name header from publisher_package

--- src/subscriber_node.cpp
+++ src/subscriber_node.cpp
@@ -1,6 +1,8 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 
+#include "publisher_package/topic_name.h"
+
 void chatterCallback(const std_msgs::String::ConstPtr& msg) {
   ROS_INFO("I Heard: [%s]", msg->data.c_str());
 }
@@ -10,7 +12,7 @@ int main(int argc, char** argv) {
 
   ros::NodeHandle nh;
 
-  ros::Subscriber sub = nh.subscribe("publisher_topic", 1000, chatterCallback);
+  ros::Subscriber sub = nh.subscribe(topic_name, 1000, chatterCallback);
 
   ros::spin();
 
